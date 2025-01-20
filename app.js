cc8bbc38f172bb09a9ebb8c715472801f495e640
Papia Das <papiadas1@in.ibm.com>
2025-01-20 10:11:17 +0530

    create login page

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require ('express');
 const app = express('app');
 const PORT = 3000;
 
+/*
+// app.get('/',(req,res) =>{
+//     res.send("Welcome to express.js");
+// } );
 
-app.get('/',(req,res) =>{
-    res.send("Welcome to express.js");
-} );
+app.get('/hello',(req,res)=>{
+    res.set("Content-Type","text/html");
+    res.status(200).send("<h1>Welcome to NODE JS</h>")
+})*/
+
+app.use(express.json());
+app.post('/',(req,res)=>{
+    const {name} = req.body;
+    console.log("Name"+ req.body.name);
+    res.send(`Welcome ${name}`);
+})
 
 app.listen(PORT,(error)=> {
     if(!error)
