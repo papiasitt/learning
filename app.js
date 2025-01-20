17aa181ecf9da5df9c998d4f77d46bc0e728a799
Papia Das <papiadas1@in.ibm.com>
2025-01-20 08:43:13 +0530

    setup nodeJs

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const app = express('app');
+const PORT = 3000;
+
+
+app.get('/',(req,res) =>{
+    res.send("Welcome to express.js");
+} );
+
+app.listen(PORT,(error)=> {
+    if(!error)
+    console.log("Server is Successfully Running, and App is listening on port "+ PORT);
+else 
+    console.log("Error occurred, server can't start", error);
+})
